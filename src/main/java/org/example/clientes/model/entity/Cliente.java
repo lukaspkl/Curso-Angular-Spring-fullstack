911f00aedc53a12256d8e489373f15ff92c86c57
lukaspkl <fohrenoficial@Live.com>
2023-01-27 05:22:31 -0300

    Aplicando Regras de Validação

--- src/main/java/org/example/clientes/model/entity/Cliente.java
+++ src/main/java/org/example/clientes/model/entity/Cliente.java
@@ -2,8 +2,11 @@ package org.example.clientes.model.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Entity
@@ -18,9 +21,12 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
