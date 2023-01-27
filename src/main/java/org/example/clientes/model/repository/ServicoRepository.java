2c2eb44ed4f693c62e3c1e3575ce4bc051daf949
lukaspkl <fohrenoficial@Live.com>
2023-01-27 03:05:18 -0300

    Criação das Interfaces Cliente e Serviço

---
+++ src/main/java/org/example/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package org.example.clientes.model.repository;
+
+import org.example.clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
