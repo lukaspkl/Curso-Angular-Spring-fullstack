87e232f982a2a589f1ae785930f2483f0c7d97c0
lukaspkl <fohrenoficial@Live.com>
2023-01-27 04:39:35 -0300

    Deletando um Ciente

--- src/main/java/org/example/clientes/rest/ClienteController.java
+++ src/main/java/org/example/clientes/rest/ClienteController.java
@@ -1,24 +1,23 @@
 package org.example.clientes.rest;
 
+import lombok.RequiredArgsConstructor;
 import org.example.clientes.model.entity.Cliente;
 import org.example.clientes.model.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import static org.springframework.http.HttpStatus.*;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
     private final ClienteRepository clienteRepository;
 
-    public ClienteController(ClienteRepository clienteRepository) {
-        this.clienteRepository = clienteRepository;
-    }
-
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     public Cliente salvar (@RequestBody Cliente cliente){
         return clienteRepository
                 .save(cliente);
@@ -30,6 +29,21 @@ public class ClienteController {
                 .findById(id)
                 .orElseThrow(()-> new ResponseStatusException(
                         HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(NO_CONTENT)
+    public void deletar (@PathVariable Integer id){
+        clienteRepository
+                .findById(id)
+                    .map(c -> {
+                        clienteRepository.delete(c);
+                        return Void.TYPE;
+                })
+                    .orElseThrow(()-> new ResponseStatusException(
+                            HttpStatus.NOT_FOUND));
+
 
     }
 }
