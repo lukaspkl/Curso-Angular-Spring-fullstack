3d33a749bf907a48659f31cbaf8fb95c380159a5
lukaspkl <fohrenoficial@Live.com>
2023-01-28 01:31:35 -0300

    Desenvolvendo formulario de clientes

--- api/src/main/java/org/example/clientes/ClientesApplication.java
+++ api/src/main/java/org/example/clientes/ClientesApplication.java
@@ -1,12 +1,7 @@
 package org.example.clientes;
 
-import org.example.clientes.model.entity.Cliente;
-import org.example.clientes.model.repository.ClienteRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
